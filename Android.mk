ad93cb841b59f0b37e5e08ff54469eb45623636a
Martin Heistermann <mh-git@sponc.de>
2013-12-08 20:35:02 +0900

    Android.mk cleanup.

--- Android.mk
+++ Android.mk
@@ -2,17 +2,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := eng
-
-#LOCAL_LDLIBS := -L/libs/achartengine-1.1.0
-#LOCAL_STATIC_JAVA_LIBRARIES := achartengine
-#LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := achartengine
-
 LOCAL_STATIC_JAVA_LIBRARIES := achartengine
-
 LOCAL_SRC_FILES := $(call all-subdir-java-files) \
-
-#LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
-
 LOCAL_PACKAGE_NAME := measurer
 
 include $(BUILD_PACKAGE)
